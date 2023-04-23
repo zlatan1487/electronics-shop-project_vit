f8a5c48a9c9c9625009084e65674f8966e16d36e
zlatan1487 <vitalik.karpukhin@list.ru>
2023-04-23 23:52:08 +0200

    modified second task "electronics-shop"

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +34,34 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = float(self.price * self.pay_rate)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        filepath = "../src/items.csv"
+        if not os.path.exists(filepath):
+            filepath = "src/items.csv"
+
+        with open(filepath, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for line in reader:
+                cls.all.append(cls(line['name'], int(line['price']), int(line['quantity'])))
+
+        return cls.all
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self._name = value
+        else:
+            raise ValueError("Длина наименования товара не может быть больше 10 символов")
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number))
+
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 
 item1 = Item("Смартфон", 10000, 20)
@@ -30,6 +32,29 @@ def test_class_item2():
     assert item2.price == 20000
 
 
-def test_get_all():
-    print(Item.all)
-    assert Item.all == [item1, item2]
+def test_long_name():
+    item3 = Item('смартфон', 10000, 5)
+    try:
+        item3.name = "СуперСмартфон"
+        assert False, "ValueError: Длина наименования товара не может быть больше 10 символов"
+    except ValueError:
+        assert True
+
+
+def test_string_to_number():
+    assert Item.string_to_number("10.0") == 10
+    assert Item.string_to_number("5") == 5
+
+
+def test_name_getter():
+    item = Item("Смартфон", 10.0, 5)
+    assert item.name == "Смартфон"
+    Item.instantiate_from_csv()
+    item_ = Item.all[0]
+    assert item_.name == "Смартфон"
+
+
+def test_length_all():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
