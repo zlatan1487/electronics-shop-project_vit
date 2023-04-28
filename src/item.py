8761ed01f4562a1099e296ac7c9bd7f282e5beef
zlatan1487 <vitalik.karpukhin@list.ru>
2023-04-28 11:27:32 +0200

    changed the third task "Electronics store"

--- src/item.py
+++ src/item.py
@@ -35,6 +35,11 @@ class Item:
         """
         self.price = float(self.price * self.pay_rate)
 
+    def __repr__(self):
+        return f'{self.__class__.__name__}{self.name ,self.price, self.quantity}'
+
+    def __str__(self):
+        return f'{self.name}'
     @classmethod
     def instantiate_from_csv(cls):
         cls.all.clear()
--- tests/test_item.py
+++ tests/test_item.py
@@ -6,6 +6,7 @@ from src.item import Item
 item1 = Item("Смартфон", 10000, 20)
 
 
+# TestCase#1 item1
 def test_class_item1():
     assert item1.name == "Смартфон"
     assert item1.price == 10000
@@ -21,6 +22,7 @@ def test_class_item1():
 item2 = Item("Ноутбук", 20000, 5)
 
 
+# TestCase#2 item2
 def test_class_item2():
     assert item2.name == "Ноутбук"
     assert item2.price == 20000
@@ -32,6 +34,8 @@ def test_class_item2():
     assert item2.price == 20000
 
 
+# TestCase#3 check long name
+
 def test_long_name():
     item3 = Item('смартфон', 10000, 5)
     try:
@@ -41,10 +45,14 @@ def test_long_name():
         assert True
 
 
+# TestCase#4 check string_to_number
+
 def test_string_to_number():
     assert Item.string_to_number("10.0") == 10
     assert Item.string_to_number("5") == 5
 
+
+# TestCase#5 check test_name_getter
 
 def test_name_getter():
     item = Item("Смартфон", 10.0, 5)
@@ -54,7 +62,21 @@ def test_name_getter():
     assert item_.name == "Смартфон"
 
 
+# TestCase#6 check length array
+
 def test_length_all():
     Item.instantiate_from_csv()
     assert len(Item.all) == 5
+
+
+# TestCase#7 check method representation
+def test_method_representation():
+    repr_ = Item("Телефон", 18000, 10)
+    assert repr(repr_) == "Item('Телефон', 18000, 10)"
+
+
+# TestCase#8 check method string
+def test_method_string():
+    string_ = Item("Yottanewton", 15000, 13)
+    assert str(string_) == 'Yottanewton'
 
